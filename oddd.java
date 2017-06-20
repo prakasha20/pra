68a7b7501909ec9c8e8d8b60d0e74f4ce077a00f
prakasha20 <prakash.a0497@gmail.com>
2017-06-20 18:28:59 +0530

    Create oddd.java

---
+++ oddd.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+class oddd
+{
+public static void main(String[] arg)
+{
+Scanner s=new Scanner(System.in);
+int a=s.nextInt();
+if(a%2==0)
+System.out.println("EVEN");
+else
+System.out.println("ODD");
+}
+}
